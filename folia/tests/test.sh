855ac3feda217d11098edc799220d2dad6a34db1
Maarten van Gompel <proycon@anaproy.nl>
2018-09-06 17:35:41 +0200

    removing non-folia tests (pynlpl legacy)

--- folia/tests/test.sh
+++ folia/tests/test.sh
@@ -15,59 +15,6 @@ cd $TESTDIR
 
 GOOD=1
 
-echo "Testing CGN">&2
-$PYTHON cgn.py
-if [ $? -ne 0 ]; then
-    echo "Test failed!!!" >&2
-    GOOD=0
-fi
-
-echo "Testing datatypes">&2
-$PYTHON datatypes.py
-if [ $? -ne 0 ]; then
-    echo "Test failed!!!" >&2
-    GOOD=0
-fi
-
-
-echo "Testing evaluation">&2
-$PYTHON evaluation.py
-if [ $? -ne 0 ]; then
-    echo "Test failed!!!" >&2
-    GOOD=0
-fi
-
-
-echo "Testing search">&2
-$PYTHON search.py
-if [ $? -ne 0 ]; then
-    echo "Test failed!!!" >&2
-    GOOD=0
-fi
-
-echo "Testing textprocessors">&2
-$PYTHON textprocessors.py
-if [ $? -ne 0 ]; then
-    echo "Test failed!!!" >&2
-    GOOD=0
-fi
-
-
-echo "Testing statistics">&2
-$PYTHON statistics.py
-if [ $? -ne 0 ]; then
-    echo "Test failed!!!" >&2
-    GOOD=0
-fi
-
-
-echo "Testing formats">&2
-$PYTHON formats.py
-if [ $? -ne 0 ]; then
-    echo "Test failed!!!" >&2
-    GOOD=0
-fi
-
 echo "Testing folia">&2
 $PYTHON folia.py
 if [ $? -ne 0 ]; then
@@ -77,13 +24,6 @@ fi
 
 echo "Testing FQL">&2
 $PYTHON fql.py
-if [ $? -ne 0 ]; then
-    echo "Test failed!!!" >&2
-    GOOD=0
-fi
-
-echo "Testing CQL">&2
-$PYTHON cql.py
 if [ $? -ne 0 ]; then
     echo "Test failed!!!" >&2
     GOOD=0
