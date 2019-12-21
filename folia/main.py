7821c4c3029b343a9fbda2632e69f77bd2ec43cf
Maarten van Gompel <proycon@anaproy.nl>
2019-12-21 13:15:53 +0100

    Fix for items() and added elements() (proycon/foliatools#7)

--- folia/main.py
+++ folia/main.py
@@ -1629,8 +1629,9 @@ class AbstractElement:
 
     def __eq__(self, other): #pylint: disable=too-many-return-statements
         """Equality method, tests whether two elements are equal.
-
         Elements are equal if all their attributes and children are equal."""
+        if self is other: return True #pythonic equality (object address)
+
         if self.doc and self.doc.debug: print("[FoLiA DEBUG] AbstractElement Equality Check - " + repr(self) + " vs " + repr(other),file=stderr)
 
         #Check if we are of the same time
@@ -2719,14 +2720,26 @@ class AbstractElement:
         """
         return sum(1 for i in self.select(Class,set,recursive,ignore,node) )
 
-    def items(self, founditems=[]): #pylint: disable=dangerous-default-value
+    def items(self, founditems=set()): #pylint: disable=dangerous-default-value
         """Returns a depth-first flat list of *all* items below this element (not limited to AbstractElement)"""
         l = []
         for e in self.data:
-            if  e not in founditems: #prevent going in recursive loops
+            if id(e) not in founditems: #prevent going in recursive loops
+                founditems.add(id(e))
                 l.append(e)
                 if isinstance(e, AbstractElement):
-                    l += e.items(l)
+                    l += e.items(l, founditems)
+        return l
+
+    def elements(self, founditems=set()): #pylint: disable=dangerous-default-value
+        """Returns a depth-first flat list of *all* elements below this element"""
+        l = []
+        for e in self.data:
+            if id(e) not in founditems: #prevent going in recursive loops
+                if isinstance(e, AbstractElement):
+                    founditems.add(id(e))
+                    l.append(e)
+                    l += e.elements(founditems)
         return l
 
     def getmetadata(self, key=None):
@@ -7239,6 +7252,13 @@ class Document(object):
         l = []
         for e in self.data:
             l += e.items()
+        return l
+
+    def elements(self):
+        """Returns a depth-first flat list of all elements in the document"""
+        l = []
+        for e in self.data:
+            l += e.elements()
         return l
 
     def xpath(self, query):
